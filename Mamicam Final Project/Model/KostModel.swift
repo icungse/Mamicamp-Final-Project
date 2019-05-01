4c2c0b4e8a1ddcd8deb8794e1b85a9d79f5124a5
Saiful <icungse@gmail.com>
2019-05-02 04:37:42 +0700

    create kost model

---
+++ Mamicam Final Project/Model/KostModel.swift
@@ -0,0 +1,32 @@
+//
+//  KostModel.swift
+//  Mamicam Final Project
+//
+//  Created by icungse on 02/05/19.
+//  Copyright © 2019 icung. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class KostModel {
+    let baseUrl = "https://esjeruks.com/api-mamicamp/public/api/room"
+    
+    var id : String = ""
+    var name : String = ""
+    var description : String = ""
+    var type : String = ""
+    var price : String = ""
+    var category : String = ""
+    var image_url : String = ""
+    
+    required init(_ object: JSON) {
+        self.id = object["id"].stringValue
+        self.name = object["name"].stringValue
+        self.description = object["description"].stringValue
+        self.type = object["type"].stringValue
+        self.price = object["price"].stringValue
+        self.category = object["category"].stringValue
+        self.image_url = object["image_url"].stringValue
+    }
+}
